425437e843ce84f572d977cedb03d2943f5b962d
Bruce Gustin <35908618+bgustin53@users.noreply.github.com>
2022-11-20 06:39:29 -0800

    Remove data file

--- Forest.java
+++ Forest.java
@@ -1,7 +1,5 @@
 import greenfoot.*;  // (Actor, World, Greenfoot, GreenfootImage)
 import java.util.List;
-import java.io.FileWriter;   // Import the FileWriter class
-import java.io.IOException;  // Import the IOException class to handle errors
 
 /**
  * Creates the world for predation events to occur and
@@ -17,13 +15,12 @@ public class Forest extends World
     private final int START_PLANTS = 800;
     private final int START_HERBIVORE = 48;
     private final int START_CARNIVORE = 3;
-    private final int STEP_REPORTING = 50;
+    private final int STEP_REPORTING = 50;     // how often to add a data line
     private final int MAX_STEPS = 5000;        //number of lines of data collected in the data file
     
     // Creates fields for writing data
-    private FileWriter myWriter;
-    private int step;                         // how often to add a data line
-    private boolean titlePrinted;
+    private int step;                         
+
     
     // Object fields
     private Animal animal;
@@ -33,7 +30,7 @@ public class Forest extends World
     public Forest() 
     {
         super(1200, 800, 1);
-        prepare();
+        prepareScene();
     }
 
     // Repeats
@@ -47,7 +44,7 @@ public class Forest extends World
      * Prepare the world for the start of the program.
      * That is: create the initial objects and add them to the world.
      */
-    private void prepare()
+    private void prepareScene()
     {     
         for(int i = 0; i < START_PLANTS; i++)
         {
@@ -72,16 +69,6 @@ public class Forest extends World
             int y = Greenfoot.getRandomNumber(getHeight());
             addObject(animal, x, y);
         }
-        
-        // Opens a new data file on the client computer
-        try
-        {
-            myWriter = new FileWriter("data.txt");
-        } catch (IOException e)
-        {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
     }
     
     
@@ -103,29 +90,9 @@ public class Forest extends World
             String carnivoreLine = "Carnivore: " + CarnivoreObjects.size();
             
             // Prints data to console 
-            System.out.format("%15s%13s%16s%18s\n", stepLine, plantLine, herbivoreLine, carnivoreLine);
+            System.out.print(stepLine + "\t" + plantLine + "\t" + herbivoreLine + "\t" +
+                             carnivoreLine + "\n");
 
-            // First prints title to data file then prints data 
-            String toPrint = "Step\tPlants(by 10s)\tHerbivores\tCarnivores\n";
-            if(!titlePrinted)
-            {
-                titlePrinted = true;
-            }
-            else
-            {
-                toPrint = step + "\t" + PlantObjects.size()/10 + "\t" + HerbivoreObjects.size() + 
-                                 "\t" + CarnivoreObjects.size() + "\n";
-            }
-            
-            // Adds next line into data file
-            try
-            {
-                myWriter.append(toPrint);
-            } catch (IOException e)
-            {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-            }
         }
     }  
     
@@ -133,16 +100,9 @@ public class Forest extends World
     {
         if(step > MAX_STEPS)
         {       
-            try
-            {
-                myWriter.close();
-                System.out.println("End of simulation");
-                Greenfoot.stop();
-            } catch (IOException e)
-            {
-                System.out.println("An error occurred.");
-                e.printStackTrace();
-            }
+            System.out.println("End of simulation");
+            Greenfoot.stop();
+          
         }
 
     }
